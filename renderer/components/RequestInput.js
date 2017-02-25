9125ae888fc64e0860c581fa34c405a2ea88e513
Akshay Nair <phenax5@gmail.com>
2017-02-25 19:45:38 +0530

    Added multiple request fields

--- renderer/components/RequestInput.js
+++ renderer/components/RequestInput.js
@@ -19,25 +19,82 @@ export default class RequestInput extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state= {
+			requestData: []
+		};
 		
 		this._onSubmitHandler= this._onSubmitHandler.bind(this);
+		this._addRequestField= this._addRequestField.bind(this);
 	}
 
-	_onSubmitHandler() {
+	_onSubmitHandler(e) {
+		e.preventDefault();
+		const $form= this.refs.formWrapper;
 
+		const data= new FormData($form);
+		console.log(data.get('url'));
+
+		const $keys= Array.from($form.querySelectorAll('.js-data__key'));
+		const $values= Array.from($form.querySelectorAll('.js-data__value'));
+
+		const requestData= {};
+
+		$keys.forEach((key, i) => {
+
+			const value= $values[i];
+			
+			if(!!key && !!value) {
+				requestData[key]= value;
+			}
+		});
+
+		console.log(requestData);
+	}
+
+	_addRequestField() {
+		this.state.requestData.push({
+			key: '',
+			value: ''
+		});
+
+		this.setState({ requestData: this.state.requestData });
 	}
 
 	render() {
+
+		const requestFields= this.state.requestData.map((data, i) => (
+			<li key={i}>
+				<input
+					type='text'
+					className='js-data__key'
+					placeholder='Key'
+					value={data.key}
+				/>
+				<input
+					type='text'
+					className='js-data__value'
+					placeholder='Value'
+					value={data.value}
+				/>
+			</li>
+		));
+
 		return (
 			<div style={RequestInput.styles.wrapper}>
-				<form onSubmit={this._onSubmitHandler}>
+				<div ref='formWrapper'>
 
 					<input
-						type='text'
+						type='text' name='url'
 						style={RequestInput.styles.input}
-						placeholder='Enter holy text'
+						placeholder='Enter URL..'
 					/>
-				</form>
+
+					<ul>{ requestFields }</ul>
+					<button onClick={this._addRequestField}>Add field</button>
+
+					<button onClick={this._onSubmitHandler}>Submit</button>
+				</div>
 			</div>
 		);
 	}
